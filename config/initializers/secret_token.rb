3d44cec8736d1af7cc68509fceeb712591278465
Ryo Sakamoto <rsakamot.0620@gmail.com>
2014-01-18 19:37:38 +0900

    delete hoge files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railbook::Application.config.secret_key_base = 'fdca5e7b79af873504521a346710322c85de6c85be3a3a4c0e6a4c16015decd1942717ca59a32459d3ae86b5b8e3a1b4b939521cd36fd61636aed483f37964b9'
